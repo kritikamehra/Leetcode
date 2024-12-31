2be789f9f19bd4e3c992e23eb7847a4bfe6763e4
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-01-01 01:04:52 +0530

    Time: 5 ms (62.37%), Space: 45.1 MB (20.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,49 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum = 0;
-        for(int i = 0; i < s.length(); i++) {
-            switch(s.charAt(i)) {
-                case 'M':
-                    sum+=1000;
-                    break;
-                case 'D':
-                    if(i != s.length()-1 && (s.charAt(i+1) == 'D'))
-                        sum-=500;
-                    else
-                        sum+=500;
-                    break;
-                case 'C':
-                    if(i != s.length()-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M'))
-                        sum-=100;
-                    else
-                        sum+=100;
-                    break;
-                case 'L':
-                    if(i != s.length()-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M' || s.charAt(i+1) == 'C'))
-                        sum-=50;
-                    else
-                        sum+=50;
-                    break;
-                case 'X':
-                    if(i != s.length()-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M' || s.charAt(i+1) == 'C' || s.charAt(i+1) == 'L'))
-                        sum-=10;
-                    else
-                        sum+=10;
-                    break;
-                case 'V':
-                    if(i != s.length()-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M' || s.charAt(i+1) == 'C' || s.charAt(i+1) == 'L' || s.charAt(i+1) == 'X'))
-                        sum-=5;
-                    else
-                        sum+=5;
-                    break;
-                case 'I':
-                    if(i != s.length()-1 && s.charAt(i+1) != 'I')
-                        sum-=1;
-                    else
-                        sum+=1;
-                    break;
+        HashMap<Character, Integer> mp= new HashMap<>();
+        mp.put('M',1000);
+        mp.put('D',500);
+        mp.put('C',100);
+        mp.put('L',50);
+        mp.put('X',10);
+        mp.put('V',5);
+        mp.put('I',1);
+
+        int n = s.length();
+        int sum = mp.get(s.charAt(n-1));
+        int val = mp.get(s.charAt(n-1));
+        for(int i = n-2; i >= 0; i--) {
+            int temp = mp.get(s.charAt(i));
+            if(temp < val) {
+                sum-=temp;
             }
+            else
+                sum+=temp;
+
+            val = temp;
         }
+
         return sum;
     }
 }
