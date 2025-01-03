ecebba48bb8bce1adf1206e46ba4d0658ca111a2
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-01-03 11:03:45 +0530

    Time: 1 ms (29.24%), Space: 42.6 MB (6.25%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,37 @@
 class Solution {
+    public void swapIfGreater(int[] nums1, int[] nums2, int left, int right) {
+        if(nums1[left] > nums2[right]) {
+            int temp = nums1[left];
+            nums1[left] = nums2[right];
+            nums2[right] = temp;
+        }
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int left = m-1, right = 0;
+        int gap = (m+n)/2 + (m+n)%2;
 
-        while(left >= 0 && right < n) {
-            if(nums1[left] > nums2[right]) {
-                int temp = nums1[left];
-                nums1[left] = nums2[right];
-                nums2[right] = temp;
+        while(gap > 0) {
+            int left = 0, right = left+gap;
+            while(right < (n+m)) {
+                if(left <m && right >= m) {
+                    swapIfGreater(nums1, nums2, left, right-m);
+                }
 
-                left--;
+                else if(left < m && right < m) {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+
+                else {
+                    swapIfGreater(nums2, nums2, left-m, right-m);
+                }
+                left++;
                 right++;
             }
 
-            else {
-                break;
-            }
+            if(gap == 1) break;
+            gap = (gap/2) + (gap%2);
         }
 
-        Arrays.sort(nums1, 0, m);
-        Arrays.sort(nums2);
-
-        for(int i = m; i < m+n; i++) {
+        for(int i = m; i < (m+n); i++) {
             nums1[i] = nums2[i-m];
         }
     }
