99198fc444e215ef49096dea83f0b96634b16701
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-01-21 23:52:17 +0530

    Time: 3 ms (90.17%), Space: 45 MB (11.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> smp = new HashMap<>();
-        HashMap<Character,Integer> tmp = new HashMap<>();
+        // HashMap<Character,Integer> smp = new HashMap<>();
+        // HashMap<Character,Integer> tmp = new HashMap<>();
 
-        for(int i =0 ; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            smp.put(ch, smp.getOrDefault(ch, 0)+1);
-        }
+        // for(int i =0 ; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     smp.put(ch, smp.getOrDefault(ch, 0)+1);
+        // }
 
-        for(int i =0 ; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            tmp.put(ch, tmp.getOrDefault(ch, 0)+1);
-        }
+        // for(int i =0 ; i < t.length(); i++) {
+        //     char ch = t.charAt(i);
+        //     tmp.put(ch, tmp.getOrDefault(ch, 0)+1);
+        // }
 
-        if(smp.equals(tmp))  return true;
+        // if(smp.equals(tmp))  return true;
 
+        // return false;
+
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        if(Arrays.equals(s1,t1))    return true;
         return false;
     }
 }
