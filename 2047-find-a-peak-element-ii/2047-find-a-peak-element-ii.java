05b526be8919bdfb8c96b6457b17f9510141c89a
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-01-27 12:18:32 +0530

    Time: 8 ms (5.62%), Space: 84.2 MB (13.3%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int n = mat.length, m = mat[0].length;
-        int ans[] = new int[2];
-        int s = -1, e = -1;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                boolean c1 = j == 0 || mat[i][j] > mat[i][j-1];
-                boolean c2 = j == m-1 ||mat[i][j] > mat[i][j+1];
-                boolean c3 = i == 0 || mat[i][j] > mat[i-1][j];
-                boolean c4 = i == n-1 || mat[i][j] > mat[i+1][j];
+        int ans[] = {-1,-1};
+        for(int i = 0; i < m*n; i++) {
+            int col = i%m;
+            int row = i/m;
+            
+            int ele = mat[row][col];
+            boolean c1 = col-1 < 0 || ele > mat[row][col - 1]; //left
+            boolean c2 = col+1 >= m || ele > mat[row][col + 1]; 
+            boolean c3 = row+1 >= n || ele > mat[row + 1][col];
+            boolean c4 = row-1 < 0 || ele > mat[row - 1][col];
 
-                if(c1 && c2 && c3 && c4) {
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;
-                }
+            if(c1 && c2 && c3 && c4) {
+                ans[0] = row;
+                ans[1] = col;
+                return ans;
             }
         }
         return ans;
