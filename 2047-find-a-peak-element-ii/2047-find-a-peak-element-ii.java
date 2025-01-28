52842b522bd8e9d2532f8304a9cf59fe7a2ff5e8
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-01-28 12:10:00 +0530

    Time: 0 ms (100%), Space: 83.9 MB (43.47%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,23 +1,29 @@
 class Solution {
-    public int[] findPeakGrid(int[][] mat) {
-        int n = mat.length, m = mat[0].length;
-        int ans[] = {-1,-1};
-        for(int i = 0; i < m*n; i++) {
-            int col = i%m;
-            int row = i/m;
-            
-            int ele = mat[row][col];
-            boolean c1 = col-1 < 0 || ele > mat[row][col - 1]; //left
-            boolean c2 = col+1 >= m || ele > mat[row][col + 1]; 
-            boolean c3 = row+1 >= n || ele > mat[row + 1][col];
-            boolean c4 = row-1 < 0 || ele > mat[row - 1][col];
-
-            if(c1 && c2 && c3 && c4) {
-                ans[0] = row;
-                ans[1] = col;
-                return ans;
+    public int findIndex(int[][] mat, int n, int m, int col) {
+        int maxele = Integer.MIN_VALUE, maxIndex = -1;
+        for(int i = 0; i < n; i++) {
+            if(maxele < mat[i][col]) {
+                maxele = mat[i][col];
+                maxIndex = i;
             }
         }
-        return ans;
+        return maxIndex;
+    }
+    public int[] findPeakGrid(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        // int ans[] = {-1,-1};
+        int low = 0, high = m - 1;
+        while(low <= high) {
+            int mid = low+(high-low)/2;
+            int maxEleIndex =  findIndex(mat, n, m, mid); // will return row number
+            // the above method finds max ele in a column therefore the top and bottom will be already checked and will be smaller than the max ele
+            // check left and right
+            int right = mid+1 < m ? mat[maxEleIndex][mid+1] : -1;
+            int left = mid-1 >= 0 ? mat[maxEleIndex][mid-1] : -1;
+            if(mat[maxEleIndex][mid] > left && mat[maxEleIndex][mid] > right) return new int[]{maxEleIndex, mid};
+            else if(mat[maxEleIndex][mid] < left)   high = mid-1;
+            else    low = mid+1;
+        }
+        return new int[]{-1,-1};
     }
 }
