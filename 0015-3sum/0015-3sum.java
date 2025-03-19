c0aa774553912b6dfd01c7187ed5faef517f19fa
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-03-19 23:29:48 +0530

    Time: 316 ms (21.84%), Space: 52.8 MB (14.12%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,33 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        
-        int n = nums.length;
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        Set<List<Integer>> list = new HashSet<>();
-        for(int i = 0 ; i < n; i++) {
-            int start = i+1, end = n-1;
-            while(start < end) {
-                List<Integer> temp = new ArrayList<>();
-                int sum = nums[i] + nums[start] + nums[end];
-                if(sum == 0) {
-                    temp.add(nums[i]);
-                    temp.add(nums[start]);
-                    temp.add(nums[end]);
-
-                    list.add(temp);
-                    start++;
-                    end--;
-                    // temp.clear();
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        int l = arr.length;
+        int i=0;
+        HashSet<List<Integer>> hs = new HashSet<>();
+        while(i<l) {
+            int j=i+1, k=l-1;
+            while(j<k) {
+                int sum = arr[i]+arr[j]+arr[k];
+                if(sum==0) {
+                    ArrayList<Integer> al = new ArrayList<>();
+                    al.add(arr[i]);
+                    al.add(arr[j]);
+                    al.add(arr[k]);
+                    hs.add(al);
+                    j++;
+                    k--;
                 }
-
                 else if(sum < 0)
-                    start++;
-
+                    j++;
                 else
-                    end--;
+                    k--;
             }
+            i++;
         }
-
-        if(list != null)
-        for(List<Integer> i: list) {
-            ans.add(i);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(List<Integer> al : hs) {
+            ans.add(al);
         }
-
         return ans;
     }
 }
