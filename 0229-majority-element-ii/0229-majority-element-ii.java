39974c246856e2b1d88eb7308760a1c427be3e96
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-06-20 05:29:15 +0530

    Time: 2 ms (99.6%), Space: 50.6 MB (22.14%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, cnt1 = 0, cnt2 = 0;
-        List<Integer> list = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        if(nums.length == 1) {
+            ans.add(nums[0]);
+            return ans;
+        }
 
-        int n = nums.length;
+        int ele1 = Integer.MAX_VALUE, cnt1 = 0;
+        int ele2 = Integer.MIN_VALUE, cnt2 = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(cnt1 == 0 && nums[i] != ele2) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else if(cnt1 == 0 && nums[i] != ele2) {
                 ele1 = nums[i];
-                cnt1 = 1;
+                cnt1++;
             }
-
             else if(cnt2 == 0 && nums[i] != ele1) {
                 ele2 = nums[i];
-                cnt2 = 1;
-            }
-
-            else if(ele1 == nums[i])
-                cnt1++;
-
-            else if(ele2 == nums[i])
                 cnt2++;
+            }
 
             else {
                 cnt1--;
@@ -28,20 +27,21 @@ class Solution {
             }
         }
 
-        cnt1 = 0;
-        cnt2 = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == ele1)
-                cnt1++;
-            else if(nums[i] == ele2)
-                cnt2++;
+        //check
+        int c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == ele1) c1++;
+            else if(nums[i] == ele2) c2++;
         }
 
-        if(cnt1 > (n/3))
-            list.add(ele1);
-        if(cnt2 > (n/3))
-            list.add(ele2);
+        int n = nums.length;
 
-        return list;
+        if(c1 > n/3)
+            ans.add(ele1);
+
+        if(c2 > n/3)
+            ans.add(ele2);
+
+        return ans;
     }
 }
