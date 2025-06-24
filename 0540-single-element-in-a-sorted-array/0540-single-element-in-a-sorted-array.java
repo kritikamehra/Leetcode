414bcb229cbe3e5de15d73b9f9dc5ca496ca3c06
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-06-24 23:36:09 +0530

    Time: 0 ms (100%), Space: 50.8 MB (13.57%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,23 +1,24 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low = 0, high = nums.length-1;
-
-        while(low <= high) {
-            int mid = low+(high-low)/2;
-
-            if((mid)%2 == 0) {
-                if(mid-1 >= 0 && nums[mid-1] == nums[mid]) high = mid-1;
-                else if(mid+1 <= nums.length-1 && nums[mid+1] == nums[mid]) low=mid+1;
+        int start = 0, end = nums.length-1;
+        if(nums.length == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        else if(nums[end-1] != nums[end]) return nums[end];
+        while(end >= start) {
+            int mid = start+(end-start)/2;
+            
+            if(mid%2 == 0) {
+                if(mid-1 >= 0 && nums[mid] == nums[mid-1]) end = mid-1;
+                else if(mid+1 < nums.length && nums[mid] == nums[mid+1]) start = mid+1;
                 else return nums[mid];
             }
+
             else {
-                if(mid+1 <= nums.length-1 && nums[mid+1] == nums[mid]) high = mid-1;
-                else if (mid-1 >= 0 && nums[mid-1] == nums[mid]) low=mid+1;
+                if(mid-1 >= 0 && nums[mid] == nums[mid-1]) start = mid+1;
+                else if(mid+1 < nums.length && nums[mid] == nums[mid+1]) end = mid-1;
                 else return nums[mid];
             }
-            // else if((mid+1)%2 == 0 && mid+1 <= nums.length-1 && nums[mid] == nums[mid+1]) high = mid-1;
-            // else return nums[mid];
         }
-        return nums[low];
+        return nums[end];
     }
 }
