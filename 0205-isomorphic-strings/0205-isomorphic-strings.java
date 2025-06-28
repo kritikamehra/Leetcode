cd777b8a2b7974d413f02a1454677a10b32d4bfa
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-06-28 11:50:14 +0530

    Time: 17 ms (34.69%), Space: 42.6 MB (52.77%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        // if(s.length() != t.length())  return false;
-        HashMap<Character,Character> mp = new HashMap<>();
-        HashSet<Character> hs = new HashSet<>();
+
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Character> smap = new HashMap<>();
+        HashMap<Character, Character> tmap = new HashMap<>();
 
         for(int i = 0; i < s.length(); i++) {
-            if(mp.containsKey(s.charAt(i))) {
-                if(t.charAt(i) != mp.get(s.charAt(i))) return false;
+            char s_ch = s.charAt(i);
+            char t_ch = t.charAt(i);
+
+            if(smap.containsKey(s_ch)) {
+                if(smap.get(s_ch) == t_ch && tmap.containsKey(t_ch) && tmap.get(t_ch) == s_ch)
+                    continue;
+                else return false;
             }
+
+            else if(tmap.containsKey(t_ch)) return false;
             else {
-                if(hs.contains(t.charAt(i)))    return false;
-                else {
-                    hs.add(t.charAt(i));
-                    mp.put(s.charAt(i),t.charAt(i));
-                }
+                smap.put(s_ch, t_ch);
+                tmap.put(t_ch, s_ch);
             }
         }
-
         return true;
     }
 }
