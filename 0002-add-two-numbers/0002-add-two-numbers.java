1e3be313582c66ff5cf48a7bedba2b3eb807fdb7
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-01 20:49:49 +0530

    Time: 1 ms (100%), Space: 44.6 MB (53.43%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,51 +9,45 @@
  * }
  */
 class Solution {
-    public ListNode sum(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode temp1 = l1, temp2 = l2;
-        ListNode ans = new ListNode();
-        ListNode dummy = ans;
         int sum = 0, carry = 0;
+        ListNode dummy = new ListNode();
+        ListNode prev = dummy;
+
         while(temp1 != null && temp2 != null) {
             sum = temp1.val + temp2.val + carry;
-            carry = sum/10;
             ListNode newnode = new ListNode(sum%10);
-            ans.next = newnode;
-            ans = newnode;
+            prev.next = newnode;
+            prev = newnode;
+            carry = sum/10;
             temp1 = temp1.next;
             temp2 = temp2.next;
         }
 
-        while(temp1 != null) {
+         while(temp1 != null) {
             sum = temp1.val + carry;
-            carry = sum/10;
             ListNode newnode = new ListNode(sum%10);
-            ans.next = newnode;
-            ans = newnode;
+            prev.next = newnode;
+            prev = newnode;
+            carry = sum/10;
             temp1 = temp1.next;
         }
 
-        while(temp2 != null) {
+         while(temp2 != null) {
             sum = temp2.val + carry;
-            carry = sum/10;
             ListNode newnode = new ListNode(sum%10);
-            ans.next = newnode;
-            ans = newnode;
+            prev.next = newnode;
+            prev = newnode;
+            carry = sum/10;
             temp2 = temp2.next;
         }
 
         if(carry != 0) {
-            while(carry != 0) { 
-                ListNode newnode = new ListNode(carry%10);
-                ans.next = newnode;
-                ans = newnode;
-                carry = carry/10;
-            }
+            ListNode newnode = new ListNode(carry);
+            prev.next = newnode;
         }
-        return dummy.next;
-    }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return sum(l1,l2);
+        return dummy.next;
     }
 }
