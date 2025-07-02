0ef8a93558212df4ee44c9206688284d8d85ce76
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-02 20:47:28 +0530

    Time: 12 ms (29.6%), Space: 56.9 MB (48.54%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -12,27 +12,65 @@ class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
 
-        ListNode node = head;
-        
-        ListNode sortedHead = sortList(head.next);
-        ListNode temp = sortedHead, prev = head;
-        
-        // check first node
-        if(temp != null && head.val <= temp.val) {
-            head.next = sortedHead;
-            return head;
+        ListNode mid = getMid(head);
+        ListNode leftlist = head;
+        ListNode rightlist = mid.next;
+        mid.next = null;
+
+        ListNode sortedLeft = sortList(leftlist);
+        ListNode sortedRight = sortList(rightlist);
+        ListNode mergeHead = merge(sortedLeft, sortedRight);
+
+        return mergeHead;
+    }
+
+    public ListNode getMid(ListNode head) {
+
+        if(head == null || head.next == null) return head; 
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        //traverse until found just greater ele
-        while(temp != null && temp.val < node.val) {
-            prev = temp;
-            temp = temp.next;
+        return slow;
+    }
+
+    public ListNode merge(ListNode left, ListNode right) {
+        ListNode ans = new ListNode();
+        ListNode curr = ans;
+        ListNode t1 = left, t2 = right;
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+
+        while(t1 != null && t2 != null) {
+            if(t1.val <= t2.val) {
+                curr.next = t1;
+                curr = curr.next;
+                t1 = t1.next;
+            }
+
+            else {
+                curr.next = t2;
+                curr = curr.next;
+                t2 = t2.next;
+            }
         }
 
-        //insert in the linkedlist
-        prev.next = node;
-        node.next = temp;
+        while(t1 != null) {
+            curr.next = t1;
+            curr = curr.next;
+            t1 = t1.next;
+        }
 
-        return sortedHead;
+        while(t2 != null) {
+            curr.next = t2;
+            curr = curr.next;
+            t2 = t2.next;
+        }
+
+        return ans.next;
     }
 }
