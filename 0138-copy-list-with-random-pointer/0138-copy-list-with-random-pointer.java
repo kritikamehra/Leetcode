e03c53dfc93882c787f0a1f5c1c4e09353e37fda
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-04 09:51:48 +0530

    Time: 0 ms (100%), Space: 44.3 MB (92.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,62 +15,36 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node ans = new Node(0);
-        Node dummy = ans;
+    
         Node temp = head;
-        
-        // head null
-        if(head == null) return null;
-
-        Node next = null;
-        // copy next pointers
         while(temp != null) {
             Node newnode = new Node(temp.val);
-            // next = temp.next;
-            dummy.next = newnode;
-            dummy = newnode;
+            newnode.next = temp.next;
+            temp.next = newnode;
+            temp = temp.next.next;
+        }
+
+        temp = head;
+        while(temp != null) {
+            Node node = temp.next;
+            if(temp.random != null)
+                node.random = temp.random.next;
+            else node.random = null;
+
+            temp = temp.next.next;
+        }
+
+        temp = head;
+        Node copydummy = new Node(0);
+        Node curr = copydummy;
+
+        while(temp != null) {
+            curr.next = temp.next;
+            curr = curr.next;
+            temp.next = temp.next.next;
             temp = temp.next;
         }
 
-        //set next pointers
-        temp = head;
-        Node t = ans.next;
-    
-        while(temp != null) {
-            next = temp.next;
-            temp.next = t;
-            temp = next;
-            
-            next = t.next;
-            t.next = temp;
-            t = next;
-        }
-
-        //add random pointer
-        temp = head;
-        while(temp != null) {
-            if(temp.next != null) {
-                if(temp.random == null)
-                    temp.next.random = null;
-                else {
-                    temp.next.random = temp.random.next;
-                }
-                temp = temp.next.next;
-            }
-        }
-
-
-        //remove next pointers
-        temp = head;
-        next = head.next;
-        while(temp != null) {
-            temp.next = next.next;
-            temp = temp.next;
-            if(next.next != null)
-            next.next = temp.next;
-            next = next.next;
-        }
-
-        return ans.next;
+        return copydummy.next;
     }
 }
