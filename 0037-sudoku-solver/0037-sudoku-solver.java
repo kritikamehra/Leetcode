2ccac604a72e8a5ad564add3cc73cbd6a40a7943
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-05 22:59:25 +0530

    Time: 381 ms (9.93%), Space: 41 MB (85.64%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -3,20 +3,15 @@ class Solution {
         solve(board);
     }
 
-    boolean solve(char[][] board) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
+    public boolean solve(char[][] board) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
                 if(board[i][j] == '.') {
-                    for(char ch = '1'; ch <= '9'; ch++) {
-
-                        if(isValid(board, i, j, ch)) {
-                            board[i][j] = ch;
-
-                            if(solve(board) == true)
-                                return true;
-
-                            else 
-                                board[i][j] = '.';
+                    for(char k = '1'; k <= '9'; k++) {
+                        if(isPossible(board, i, j, k)) {
+                            board[i][j] = k;
+                            if(solve(board)) return true;
+                            board[i][j] = '.';
                         }
                     }
                     return false;
@@ -26,11 +21,11 @@ class Solution {
         return true;
     }
 
-    boolean isValid(char[][] board, int row, int col, char ch) {
-        for(int i = 0; i < 9; i++) {
-            if(board[row][i] == ch) return false;
-            if(board[i][col] == ch) return false;
-            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == ch) return false;
+    public boolean isPossible(char[][] board, int i, int  j, char k) {
+        for(int x = 0; x < board.length; x++) {
+            if(board[x][j] == k) return false;
+            if(board[i][x] == k) return false;
+            if(board[3 * (i/3) + (x/3)][3 * (j/3) +(x%3)] == k) return false;
         }
         return true;
     }
