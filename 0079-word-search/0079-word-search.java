80bad6c0bc1da47ba8e54f624ee525d24a5fb711
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-06 00:06:12 +0530

    Time: 166 ms (33.24%), Space: 45.2 MB (11.96%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,40 @@
 class Solution {
-    public boolean helper(char[][] board, String word, int i, int j, String drxn) {
-        if(word.length() == 0)  return true;
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
-        //mark as visited
-        char temp = board[i][j];
-        board[i][j] = '.';
-
-        boolean left = false, right = false, bottom = false, top = false;
-
-        if(i-1 >= 0 && board[i-1][j] == word.charAt(0)) {
-            left = helper(board, word.substring(1), i-1, j, "right");
-        }
-
-        if(i+1 < board.length && board[i+1][j] == word.charAt(0)) {
-            right = helper(board, word.substring(1), i+1, j, "left");
-        }
-
-        if(j-1 >= 0 && board[i][j-1] == word.charAt(0)) {
-            top = helper(board, word.substring(1), i, j-1, "bottom");
-        }
-
-        if(j+1 < board[i].length && board[i][j+1] == word.charAt(0)) {
-            bottom = helper(board, word.substring(1), i, j+1, "top");
-        }
-
-        board[i][j] = temp;
-        return left || right || top || bottom;
-    }
     public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i = 0 ; i < board.length; i++) {
             for(int j = 0; j < board[i].length; j++) {
-                 if(board[i][j] == word.charAt(0)) {
-                    boolean check = helper(board, word.substring(1), i, j, "");
-                    if(check)   return true;
-                 }
+                if(board[i][j] == word.charAt(0)) {
+                    if(solve(board, visited, word.substring(1), i, j)) return true;
+                }
             }
         }
+        return false;
+    }
 
+    public boolean solve(char[][] board, boolean[][] visited, String word, int row, int col) {
+        if(word.length() == 0) return true;
+        if(row >= board.length || col >= board[0].length) return false;
+
+        visited[row][col] = true;
+
+        //right
+        if(col+1 < board[row].length && board[row][col+1] == word.charAt(0) && visited[row][col+1] == false) {
+            if(solve(board,visited,word.substring(1),row,col+1)) return true;
+        }
+        //down
+            if(row+1 < board.length && board[row+1][col] == word.charAt(0) && visited[row+1][col] == false) {
+            if(solve(board,visited,word.substring(1),row+1,col)) return true;
+        }
+        //left
+            if(col-1 >= 0 && board[row][col-1] == word.charAt(0) && visited[row][col-1] == false) {
+            if(solve(board,visited,word.substring(1),row,col-1)) return true;
+        }
+        //up
+            if(row-1 >= 0 && board[row-1][col] == word.charAt(0) && visited[row-1][col] == false) {
+            if(solve(board,visited,word.substring(1),row-1,col)) return true;
+        }
+
+        visited[row][col] = false;
         return false;
     }
 }
