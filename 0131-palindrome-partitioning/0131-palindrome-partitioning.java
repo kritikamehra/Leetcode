f812ac674b754150f4e759fed842ba2f24a922a7
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-07 08:14:12 +0530

    Time: 16 ms (7.59%), Space: 57.1 MB (44.53%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,36 +1,33 @@
 class Solution {
-    public boolean palindrome(String temp) {
-        int i = 0, j = temp.length()-1;
-        while(i<j) {
-            if(temp.charAt(i) != temp.charAt(j)) return false;
+    public boolean isPalindrome(String str) {
+        int i = 0, j = str.length()-1;
+        while(i < j) {
+            if(str.charAt(i) != str.charAt(j)) return false;
             i++;
             j--;
         }
-
         return true;
     }
-    public void helper(String s, List<List<String>> ans, List<String> str, String temp, int index) {
-
-        if(index == s.length()) {
-            ans.add(new ArrayList<>(str));
+    public void solve(List<List<String>> ans, List<String> temp, String s, int index) {
+        if(index >= s.length()) {
+            ans.add(new ArrayList<>(temp));
             return;
         }
 
+        String str = "";
         for(int i = index; i < s.length(); i++) {
-            temp = temp+s.charAt(i);
-            boolean check = palindrome(temp);
-            if(check) {
-                str.add(temp);
-                helper(s, ans, str, "", i+1);
-                str.remove(str.size()-1);
+            str+=s.charAt(i);
+            if(isPalindrome(str)) {
+                temp.add(str);
+                solve(ans, temp, s, i+1);
+                temp.remove(temp.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        List<String> str = new ArrayList<>();
-        String temp = "";
-        helper(s, ans, str, temp, 0);
+        List<String> temp = new ArrayList<>();
+        solve(ans, temp, s, 0);
         return ans;
     }
 }
