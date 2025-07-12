b9cb8f640c74e283c0edb6f5796c430037afbfa6
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-12 15:07:16 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int helper(int n, int[] dp) {
+    public int helper(int n) {
+        int[] dp = new int[n+1];
+        
         if(n <= 1) return 1;
-        if(dp[n] != -1) return dp[n];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        int onestep = helper(n-1, dp);
-        int twostep = helper(n-2, dp);
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
 
-        return dp[n] = onestep+twostep;
+        return dp[n];
     }
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
+        return helper(n);
     }
 }
