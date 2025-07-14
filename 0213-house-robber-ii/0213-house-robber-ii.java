ea3c61cf71e7dfb593b2536b7dc5c2897c4fa2a0
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-15 05:07:57 +0530

    Time: 0 ms (100%), Space: 41.5 MB (17.86%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,21 +1,25 @@
 class Solution {
-    public int helper(int[] nums, int start, int end, int[] dp) {
+    public int helper(int[] nums, int start, int end) {
         
-        if(end < start) return 0; 
-        if(dp[end] != -1) return dp[end];
+        int len = end-start+1;
+        int[] dp = new int[len];
 
-        int h1 = nums[end] + helper(nums, start, end-2, dp);
-        int h2 = helper(nums, start, end-1, dp);
+        dp[0] = nums[start];
+        if(len == 1) return dp[0];
+        dp[1] = Math.max(nums[start], nums[start+1]);
+        for(int i = start+2; i <= end; i++) {
+            int h1 = nums[i] + dp[i-start-2];
+            int h2 = dp[i-start-1];
+            dp[i-start] = Math.max(h1, h2);
+        }
 
-        return dp[end] = Math.max(h1, h2);
+        return dp[len-1];
     }
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        int h1 = helper(nums, 0, nums.length-2, dp);
-        Arrays.fill(dp, -1);
-        int h2 = helper(nums, 1, nums.length-1, dp);
+        
+        int h1 = helper(nums, 0, nums.length-2);
+        int h2 = helper(nums, 1, nums.length-1);
         return Math.max(h1, h2);
     }
 }
