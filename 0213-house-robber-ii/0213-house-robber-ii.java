7beaad99ee3b9604569888f86e8ed12944206292
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-15 20:16:11 +0530

    Time: 0 ms (100%), Space: 40.9 MB (80.83%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int helper(int[] nums, int start, int end) {
-    
-        int prev1 = 0;
-        int prev2 = 0;
-        for(int i = start; i <= end; i++) {
-            int curr = Math.max(prev1, prev2+nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
+    public int helper(int[] nums, int start, int end, int dp[]) {
+        if(start > end) return 0;
+        if(start == end) return nums[start];
+        if(dp[end] != -1) return dp[end];
 
-        return prev1;
+        int pick = nums[end] + helper(nums, start, end-2, dp);
+        int notpick = helper(nums, start, end-1, dp);
+
+        return dp[end] = Math.max(pick, notpick);
     }
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        
-        int h1 = helper(nums, 0, nums.length-2);
-        int h2 = helper(nums, 1, nums.length-1);
-        return Math.max(h1, h2);
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        int left = helper(nums, 0, n-2, dp);
+        Arrays.fill(dp, -1);
+        int right = helper(nums, 1, n-1, dp);
+        return Math.max(left, right);
     }
 }
