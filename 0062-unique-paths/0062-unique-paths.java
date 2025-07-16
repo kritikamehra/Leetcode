3f8186fbc319c5dcd0651e25a90acf5d45985348
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-16 10:10:57 +0530

    Time: 0 ms (100%), Space: 40.7 MB (36.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int helper(int m, int n, int[][] dp) {
-        if(m == 0 && n == 0) return 1;
+    public int helper(int m, int n) {
+        int dp[][] = new int[m][n];
+        dp[0][0] = 1;
 
-        if(m < 0 || n < 0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
+        //first row and col
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
 
-        //left
-        int left = helper(m, n-1, dp);
-        //up
-        int up = helper(m-1, n, dp);
+        for(int i = 1; i < m; i++) { 
+            for(int j = 1; j < n; j++) {
+                int left = dp[i][j-1];
+                int up = dp[i-1][j];
 
-        return dp[m][n] = left+up;
+                dp[i][j] = left+up;
+            }
+        }
+        return dp[m-1][n-1];
     }
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return helper(m-1, n-1, dp);
+        return helper(m, n);
     }
 }
