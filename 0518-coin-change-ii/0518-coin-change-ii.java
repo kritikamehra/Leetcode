b4644f31021c39e3f1ad673c52f897e111ad1314
Kritika Mehra <138287063+kritikamehra@users.noreply.github.com>
2025-07-19 22:34:30 +0530

    Time: 9 ms (61.28%), Space: 48.7 MB (61.41%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,21 +1,32 @@
 class Solution {
-    public int helper(int amount, int[] coins, int index, int[][] dp) {
-        if(index >= coins.length) return 0;
-        if(amount == 0) return 1;
-        if(amount < 0) return 0;
-        if(dp[index][amount] != -1) return dp[index][amount];
-        int sum = 0;
-        for(int i = index; i < coins.length; i++) {
-            sum += helper(amount-coins[i], coins, i, dp);
+    public int helper(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // any coin 0 amount 1 way take  nothing
+        }
+        for(int i = 0; i <= amount; i++) {
+            dp[0][i] = 0; // 0 coin any amount 0 ways
         }
 
-        return dp[index][amount] = sum;
+        dp[0][0] = 1; // 0 coin 0 amount 1 way
+
+        for(int coin = n-1; coin >= 0; coin--) {
+            for(int amt = 1; amt <= amount; amt++) {
+                if(coins[coin] > amt) dp[coin][amt] = dp[coin+1][amt]; //coin val > amt
+                else { 
+                    // pick
+                    int pick = dp[coin][amt-coins[coin]];
+                    //not pick 
+                    int notpick = dp[coin+1][amt];
+                    dp[coin][amt] = pick+notpick;
+                }
+            }
+        }
+        return dp[0][amount];
     }
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int i = 0; i < coins.length; i++) {
-            Arrays.fill(dp[i],-1);
-        }
-        return helper(amount, coins, 0, dp);
+        return helper(amount, coins);
     }
 }
